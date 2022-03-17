b2d81611f82e3c18e1de8e5637e740c84b361a26
Ivan Tershukov <plopr@ya.ru>
2022-03-17 21:31:01 +0300

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,103 @@
+import { exec }  from 'child_process';
+import * as readline  from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('close', function () {
+  console.log('\nBYE BYE !!!');
+  process.exit(0);
+});
+
+function execCommand(command){
+  return new Promise<string>((res, rej) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        rej(error)
+        return;
+      }
+      if(stderr) {
+        rej(error)
+        return;
+      }
+      if(stdout) {
+        res(stdout)
+        return;
+      }
+      res();
+    });
+  })
+}
+
+async function ask(question: string, defaultValue?: string) {
+  return new Promise<string>((res, rej) => {
+    try {
+      rl.question(question, function(answer) {
+        res(answer);
+      });
+      if (defaultValue){
+        rl.write(defaultValue);
+      }
+    } catch (e){
+      rej(e)
+    }
+  })
+}
+
+const workTypes: string[] = ['FEATURE', 'FIX', 'REFACTOR'];
+const workStates: string[] = ['In progress', 'Peer review'];
+
+(async ()=>{
+  const currentBranch = await execCommand('git rev-parse --abbrev-ref HEAD')
+  const prevComment = await execCommand('git log -1 | grep -o "[A-Z]: [A-Za-z ]*[^ #]" | cut -c 4-')
+  let targetBranch = 'dev'
+  let stateIndex = '1'
+
+  const comment = await ask('Comment: ', prevComment);
+  const spent = await ask('Spent time (30m|h): ', '');
+  const typeIndex = await ask('Work type (0 - FIX, 1 - FEATURE, 2 - REFACTOR): ', '1');
+  const type = workTypes[typeIndex] || workTypes[0]
+  stateIndex = await ask('State (0 - In progress, 1 - Peer review): ', stateIndex);
+  const state = workStates[stateIndex] || workStates[1];
+  const branch = await ask('Current branch: ', currentBranch.slice(0, -1));
+  targetBranch = await ask('Target branch: ', targetBranch);
+  const mrTitle = await ask('MR title (fill if need create a new request or update an exist): ', '');
+
+  if (targetBranch != currentBranch && branch != currentBranch){
+    await execCommand('git pull')
+    await execCommand(`git checkout ${targetBranch}`)
+  }
+
+  if (currentBranch != branch){
+    await execCommand(`git checkout -b ${branch} || git checkout ${branch}`)
+  }
+
+  if (currentBranch != branch){
+    await execCommand(`git checkout -b ${branch} || git checkout ${branch}`)
+  }
+
+  await execCommand(`git commit -a -m"${type}: ${comment} #${branch} State ${state} work Development ${spent}"`)
+
+  if (mrTitle){
+    await execCommand(`git push --set-upstream origin ${branch}`)
+  } else {
+    await execCommand(`git push -o merge_request.create -o merge_request.title="#${branch}: ${mrTitle}" -o merge_request.target=${targetBranch} --set-upstream origin ${branch}`)
+  }
+
+// # Auto sync with target branch
+// #if [[${TARGET_BRANCH} != ${BRANCH}]]
+// #then
+// #    git pull origin ${TARGET_BRANCH}
+// #    git push
+// #fi
+  rl.close();
+})()
+
+
+////////////////////////////////////
+// OLD VERSION//////////////////////
 // #!/bin/bash
 // function readinput() {
 //   local CLEAN_ARGS=""
@@ -71,48 +171,3 @@
 // #    git pull origin ${TARGET_BRANCH}
 // #    git push
 // #fi
-const { exec } = require('child_process');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-
-rl.question('What is your name ? ', function (name) {
-  rl.question('Where do you live ? ', function (country) {
-    console.log(`${name}, is a citizen of ${country}`);
-    rl.close();
-  });
-});
-
-rl.on('close', function () {
-  console.log('\nBYE BYE !!!');
-  process.exit(0);
-});
-
-function execCommand(command){
-  return new Promise((res, rej) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        rej(error)
-        return;
-      }
-      if(stderr) {
-        rej(error)
-        return;
-      }
-      if(stdout) {
-        res(stdout)
-        return;
-      }
-      res();
-    });
-  })
-}
-
-(async ()=>{
-  const currentBranch = await execCommand('git rev-parse --abbrev-ref HEAD')
-
-})()
